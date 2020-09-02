1a6ce96ebb014b5203dff855119409ed515b317d
Kevin <kyrodriguez97@gmail.com>
2020-09-01 19:03:53 -0500

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Rabbit
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.generators do |g|
+      g.template_engine nil #to skip views
+      g.test_framework  nil #to skip test framework
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
+  end
+end
